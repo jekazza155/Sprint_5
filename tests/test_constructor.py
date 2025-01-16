abe0ae937f328353aa7933e44969472b5c07633c
Evgeny Shilov <jekazza155@yandex.ru>
2025-01-16 15:27:54 +0300

    COMMIT

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -4,31 +4,22 @@ from urls import URLS
 
 class TestConstructorPage:
     def test_transition_to_bun_success(self, driver):
-
         driver.get(URLS.MAIN_PAGE_URL)
         driver.find_element(*MainPageLocators.sauces_btn).click()
         driver.find_element(*MainPageLocators.bun_btn).click()
-        bun_text = driver.find_element(*MainPageLocators.bun).text
-        bun_displayed = driver.find_element(*MainPageLocators.bun_ul).is_displayed()
+        bun_class = driver.find_element(*MainPageLocators.active_bun_btn).get_attribute('class')
+        assert 'tab_tab_type_current__2BEPc' in bun_class
 
-        assert bun_text == 'Булки' and bun_displayed
 
 
     def test_transition_to_sauces_success(self, driver):
-
         driver.get(URLS.MAIN_PAGE_URL)
         driver.find_element(*MainPageLocators.sauces_btn).click()
-        souces = driver.find_element(*MainPageLocators.sauces).text
-        souces_displayed = driver.find_element(*MainPageLocators.sauces_ul).is_displayed()
-
-        assert souces == 'Соусы' and souces_displayed
-
+        sauces_class = driver.find_element(*MainPageLocators.active_sauces_btn).get_attribute('class')
+        assert 'tab_tab_type_current__2BEPc' in sauces_class
 
     def test_transition_to_topping_success(self, driver):
-
         driver.get(URLS.MAIN_PAGE_URL)
         driver.find_element(*MainPageLocators.toppings_btn).click()
-        topping = driver.find_element(*MainPageLocators.topping).text
-        topping_displayed = driver.find_element(*MainPageLocators.topping_ul).is_displayed()
-
-        assert topping == 'Начинки' and topping_displayed
+        toppings_class = driver.find_element(*MainPageLocators.active_toppings_btn).get_attribute('class')
+        assert 'tab_tab_type_current__2BEPc' in toppings_class
